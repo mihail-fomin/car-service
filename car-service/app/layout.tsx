a5840d901f90b829925a32723501bfbe1a1eb87f
Mihail Fomin <m.fomin@quickresto.ru>
2025-02-21 14:15:56 +0500

    Added Roboto font

--- car-service/app/layout.tsx
+++ car-service/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body className={`${roboto.variable} antialiased`}>
+            {children}
+        </body>
     </html>
   );
 }
