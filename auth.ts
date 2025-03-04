bb6a87424ed1304e22f2c31c2437aff12bdd94a7
Mihail Fomin <m.fomin@quickresto.ru>
2025-03-04 18:02:44 +0500

    edited folder structure

---
+++ auth.ts
@@ -0,0 +1,26 @@
+import type {
+    GetServerSidePropsContext,
+    NextApiRequest,
+    NextApiResponse,
+} from "next"
+import type { NextAuthOptions } from "next-auth"
+import { getServerSession } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+  export const config = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+  } satisfies NextAuthOptions
+  
+  export function auth(
+    ...args:
+      | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+      | [NextApiRequest, NextApiResponse]
+      | []
+  ) {
+    return getServerSession(...args, config)
+  }
