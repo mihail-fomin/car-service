c256571842153dee7b0ac60e0c36e3085f9e6f14
Mihail Fomin <m.fomin@quickresto.ru>
2025-03-07 15:42:41 +0500

    feat: Added maintenanca types route

---
+++ app/api/maintenance-types/route.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/prisma/seed'
+import { NextResponse } from 'next/server'
+ 
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+    const types = await prisma.maintenanceType.findMany()
+
+    return NextResponse.json(types)
+}
